155368ca3d8b2f3e43f0de54d1da314a2466a4a5
Johnny Vega <johnny.vega@me.com>
2019-12-09 21:18:13 -0600

    iOS minimum required version update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TransmissionRPC",
     platforms: [
-        .iOS(.v11),
+        .iOS(.v13),
         .macOS(.v10_15),
     ],
     products: [
