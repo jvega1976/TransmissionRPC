4db556432c84e275c5c9542038c0f8855b132ee2
Johnny Vega <johnny.vega@me.com>
2020-07-14 12:38:49 -0500

    Commit

--- Tests/TransmissionRPCTests/TransmissionRPCTests.swift
+++ Tests/TransmissionRPCTests/TransmissionRPCTests.swift
@@ -98,7 +98,66 @@ class TransmissionRPCTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
+    func testExample2() {
+        let torrentFields = [
+            JSONKeys.activityDate,
+            JSONKeys.editDate,
+            JSONKeys.addedDate,
+            JSONKeys.startDate,
+            JSONKeys.bandwidthPriority,
+            JSONKeys.comment,
+            JSONKeys.creator,
+            JSONKeys.dateCreated,
+            JSONKeys.doneDate,
+            JSONKeys.downloadDir,
+            JSONKeys.downloadedEver,
+            JSONKeys.downloadLimit,
+            JSONKeys.downloadLimited,
+            JSONKeys.error,
+            JSONKeys.errorString,
+            JSONKeys.eta,
+            JSONKeys.hashString,
+            JSONKeys.haveUnchecked,
+            JSONKeys.haveValid,
+            JSONKeys.honorsSessionLimits,
+            JSONKeys.id,
+            JSONKeys.isFinished,
+            JSONKeys.name,
+            JSONKeys.peer_limit,
+            JSONKeys.peersConnected,
+            JSONKeys.peersGettingFromUs,
+            JSONKeys.peersSendingToUs,
+            JSONKeys.percentDone,
+            JSONKeys.pieceCount,
+            JSONKeys.pieceSize,
+            JSONKeys.queuePosition,
+            JSONKeys.rateDownload,
+            JSONKeys.rateUpload,
+            JSONKeys.recheckProgress,
+            JSONKeys.secondsDownloading,
+            JSONKeys.secondsSeeding,
+            JSONKeys.seedIdleLimit,
+            JSONKeys.seedIdleMode,
+            JSONKeys.seedRatioLimit,
+            JSONKeys.seedRatioMode,
+            JSONKeys.status,
+            JSONKeys.totalSize,
+            JSONKeys.uploadedEver,
+            JSONKeys.uploadLimit,
+            JSONKeys.uploadLimited,
+            JSONKeys.uploadRatio
+        ]
+        print(torrentFields)
+    }
     
+    func testExample1() {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        let data = Data("{\"arguments\":{\"torrents\":[{\"activityDate\":1594605188,\"addedDate\":1594597798,\"bandwidthPriority\":0,\"comment\":\"\",\"creator\":\"uTorrent/2210\",\"dateCreated\":1594243975,\"doneDate\":1594597922,\"downloadDir\":\"/volume4/video/Downloads\",\"downloadLimit\":100,\"downloadLimited\":false,\"downloadedEver\":440116730,\"error\":0,\"errorString\":\"\",\"eta\":-1,\"hashString\":\"03ab06ecf58a365369c7e55e3cbaca753cf3f21f\",\"haveUnchecked\":0,\"haveValid\":432460792,\"honorsSessionLimits\":true,\"id\":227,\"isFinished\":false,\"name\":\"[HIS Video] Chip Off the Old Block (1984).mp4\",\"peer-limit\":300,\"peersConnected\":0,\"peersGettingFromUs\":0,\"peersSendingToUs\":0,\"percentDone\":1,\"pieceCount\":825,\"pieceSize\":524288,\"queuePosition\":225,\"rateDownload\":0,\"rateUpload\":0,\"recheckProgress\":0,\"secondsDownloading\":134,\"secondsSeeding\":11442,\"seedIdleLimit\":1440,\"seedIdleMode\":0,\"seedRatioLimit\":1.2999,\"seedRatioMode\":0,\"startDate\":1594597798,\"status\":0,\"totalSize\":432460792,\"uploadLimit\":100,\"uploadLimited\":false,\"uploadRatio\":0.1332,\"uploadedEver\":58639850}]},\"result\":\"success\"}".utf8)
+        let response = try? decoder.decode(JSONTorrents.self, from: data)
+        let torrents = response?.arguments.torrents
+        dump(torrents?.first)
+    }
 
     func testExample() {
         // This is an example of a functional test case.
